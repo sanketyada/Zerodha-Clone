98359f9752de6d86c616fe18402cb6b2609eaedb
sanket Yadav <yadavsanket663@gmail.com>
2025-11-15 09:52:59 +0530

    Refactor Landing Pages with structured components and improved styling

--- frontend/src/LandingPages/Navbar.jsx
+++ frontend/src/LandingPages/Navbar.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
-  )
+      <nav class="navbar navbar-expand-lg bg-body-tertiary boredr-bottom" style={{backgroundcolor:"#fff"}}>
+        <div class="container p-2">
+          <a class="navbar-brand" href="#">
+            <img src="media/logo.svg" style={{width:"25%"}} alt="" />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse navbarleft" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active  fw-normal" aria-current="page" href="#">
+                  SignUp
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active fw-normal" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active fw-normal" href="#">
+                  Product
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active fw-normal" href="#">
+                  Priceing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active fw-normal" href="#">
+                  Priceing
+                </a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+            </form>
+          </div>
+        </div>
+      </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/LandingPages/OpenAccount.jsx
+++ frontend/src/LandingPages/OpenAccount.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function OpenAccout() {
   return (
-    <div>
-      OpneAoount
+    <div className="container text-center mt-5 p-5">
+      <h1 className=" my-4 fs-3">Open a Zerodha account</h1>
+      <p className="my-4 text-muted fs-5 fw-normal">
+       Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.
+      </p>
+      <button
+        style={{ width: "20%", margin: "0 auto" }}
+        className="p-3 btn btn-primary fs-5 mb-5"
+      >
+        Sign up for free
+      </button>
     </div>
-  )
+  );
 }
 
-export default OpenAccout
+export default OpenAccout;
